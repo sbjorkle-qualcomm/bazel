bd5db80f3be576197bb464fd675ca4e74bf2b589
salma-samy <salmasamy@google.com>
2023-05-30 17:18:41 +0200

    add test with error mode

--- src/test/py/bazel/bzlmod/bazel_lockfile_test.py
+++ src/test/py/bazel/bzlmod/bazel_lockfile_test.py
@@ -275,5 +275,28 @@ class BazelLockfileTest(test_base.TestBase):
     _, stdout, stderr = self.RunBazel(['build', '@hello//:all'])
     self.assertNotIn("Hello from the other side!", ''.join(stderr))
 
+  def testModuleExtensionWithLockfileErrorMode(self):
+    self.ScratchFile('MODULE.bazel', [
+      'lockfile_ext = use_extension("extension.bzl", "lockfile_ext")',
+      'use_repo(lockfile_ext, "hello")',
+    ])
+    self.ScratchFile('BUILD.bazel')
+    self.ScratchFile('extension.bzl', [
+      'def _repo_rule_impl(ctx):',
+      '    ctx.file("WORKSPACE")',
+      '    ctx.file("BUILD", "filegroup(name=\\"lala\\")")',
+      'repo_rule = repository_rule(implementation = _repo_rule_impl)',
+      'def _module_ext_impl(ctx):',
+      '    print("Hello from the other side!")',
+      '    repo_rule(name= "hello")',
+      'lockfile_ext = module_extension(implementation = _module_ext_impl)',
+    ])
+    _, _, stderr = self.RunBazel(['build', '@hello//:all'])
+    self.assertIn("Hello from the other side!", ''.join(stderr))
+    self.RunBazel(['shutdown'])
+    _, _, stderr = self.RunBazel(['build', '--lockfile_mode=error',
+                                  '@hello//:all'])
+    self.assertNotIn("Hello from the other side!", ''.join(stderr))
+
 if __name__ == '__main__':
   unittest.main()
